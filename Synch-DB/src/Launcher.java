a3498cf4d4b1e1fe607a7247af22fad94906831f
Dominik Hillmann <dominik@dominik-H81H3-EM2>
2019-02-15 23:35:31 +0100

    Configured Dropbox API

--- Synch-DB/src/Launcher.java
+++ Synch-DB/src/Launcher.java
@@ -1,9 +1,33 @@
+import java.io.IOException;
+import com.dropbox.core.DbxException;
+import com.dropbox.core.DbxRequestConfig;
+import com.dropbox.core.v2.DbxClientV2;
+import com.dropbox.core.v2.files.DbxUserFilesRequests;
+import com.dropbox.core.v2.files.ListFolderBuilder;
+import com.dropbox.core.v2.users.FullAccount;
 
 public class Launcher {
-
-	public static void main(String[] args) {
-		String x = "Das ist ein Test";
-		System.out.println(x);
+	
+	private static final String TOKEN = DropboxAccessToken.getToken();
+	
+	public static void main(String[] args) throws DbxException, IOException {
+		System.out.println(TOKEN);
+		String s = "Das ist ein Test";
+		System.out.println(s);
+		System.out.println("Ein weiterer Test".length());
+		
+		// First try to import and use the Dropbox library properly.
+        DbxRequestConfig config = DbxRequestConfig.newBuilder("dropbox/java-tutorial").build();
+        System.out.println(config.getClientIdentifier());
+        DbxClientV2 client = new DbxClientV2(config, TOKEN);
+        
+        // Get current account info
+        FullAccount account = client.users().getCurrentAccount();
+        System.out.println(account.getName().getDisplayName());
+        System.out.println(account.getTeamMemberId());
+        
+        // try to find out what's in the root folder
+        // DbxUserFilesRequests request = new DbxUserFilesRequests("/");
 	}
 	
 }
